55d2889c007119fbf9b5a03b495cb084df32730c
hanyugeon <hanyugeon@naver.com>
2021-07-15 20:02:06 +0900

    21.07.15_first test code_part01

---
+++ src/main/java/com/han/book/springboot/Application.java
@@ -0,0 +1,10 @@
+package com.han.book.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {      // 앞으로 만들 프로젝트의 '메인 클래스'
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
