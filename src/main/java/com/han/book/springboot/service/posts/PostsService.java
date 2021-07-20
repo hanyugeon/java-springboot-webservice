012358c07ae807f0965c78d79ce46e4f591cefc7
hanyugeon <hanyugeon@naver.com>
2021-07-20 22:13:59 +0900

    21.07.20_Make API?(등록) (Dto, Controllter, Service)

---
+++ src/main/java/com/han/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.han.book.springboot.service.posts;
+
+import com.han.book.springboot.domain.posts.PostsRepository;
+import com.han.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
