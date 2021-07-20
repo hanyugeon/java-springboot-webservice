2f7904e5aac7b1df01c6a8499260a5d0f789c198
hanyugeon <hanyugeon@naver.com>
2021-07-20 23:23:26 +0900

    21.07.20_Make API?(수정) (Dto, Controller, Service)

---
+++ src/main/java/com/han/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.han.book.springboot.web.dto;
+
+import com.han.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
