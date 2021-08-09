e65c16539895389d8e6b8cd97d5461df1516a912
hanyugeon <hanyugeon@naver.com>
2021-08-09 19:44:34 +0900

    21.08.09_Google Login Improvements with Annotation(구글 로그인시 사용자 정보표시가 안되던 문제 수정)

--- src/main/java/com/han/book/springboot/web/IndexController.java
+++ src/main/java/com/han/book/springboot/web/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
         if (user != null) {
-            model.addAttribute("username", user.getName());
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
