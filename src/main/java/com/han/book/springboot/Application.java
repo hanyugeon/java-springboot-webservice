c00a2da3ce1c8bd04a2dbff1d41c93986b97e101
hanyugeon <hanyugeon@naver.com>
2021-08-14 23:48:34 +0900

    21.08.14_Add Spring Security at Test code

--- src/main/java/com/han/book/springboot/Application.java
+++ src/main/java/com/han/book/springboot/Application.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+// @EnableJpaAuditing가 삭제됨 // JPA Auditing (비)활성화
 @SpringBootApplication
 public class Application {      // 앞으로 만들 프로젝트의 '메인 클래스'
     public static void main(String[] args) {
